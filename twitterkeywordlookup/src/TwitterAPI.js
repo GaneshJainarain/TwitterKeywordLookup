54259bbd751810318678fd0314dfed7508bc2db9
Manny <sancheze0450@gmail.com>
2020-12-05 22:02:14 -0500

    was able to retrieve spi keys form .env file

--- twitterkeywordlookup/src/TwitterAPI.js
+++ twitterkeywordlookup/src/TwitterAPI.js
@@ -3,14 +3,17 @@
 var Twitter = require('twitter');
 require('dotenv/config');
 
-const ApiKey = process.env.ApiKey
-const ApiSecretKey = process.env.ApiSecretKey
-const AccessToken = process.env.AccessToken
-const AccessTokenSecret = process.env.AccessTokenSecret
+const TWITTER_API_KEY = process.env.TWITTER_API_KEY
+const TWITTER_API_SECRET_KEY = process.env.TWITTER_API_SECRET_KEY
+const TWITTER_ACCESS_TOKEN = process.env.TWITTER_ACCESS_TOKEN
+const TWITTER_ACCESS_TOKEN_SECRET = process.env.TWITTER_ACCESS_TOKEN_SECRET
 
 var client = new Twitter({
-    consumer_key: ApiKey,
-    consumer_secret: ApiSecretKey,
-    access_token_key: AccessToken,
-    access_token_secret: AccessTokenSecret
+    consumer_key: TWITTER_API_KEY,
+    consumer_secret: TWITTER_API_SECRET_KEY,
+    access_token_key: TWITTER_ACCESS_TOKEN,
+    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
 });
+
+console.log(`api key: ${process.env.TWITTER_API_KEY}`)
+console.log('api secret:', process.env.TWITTER_API_SECRET_KEY)
