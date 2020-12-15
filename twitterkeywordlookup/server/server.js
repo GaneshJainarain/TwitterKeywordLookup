87d18fd0511b950fa43a0f6d9359845df6d0fea8
Manny <sancheze0450@gmail.com>
2020-12-15 00:58:08 -0500

    new server code

--- twitterkeywordlookup/server/server.js
+++ twitterkeywordlookup/server/server.js
@@ -1,22 +1,221 @@
+// const express = require("express");
+// const oauth = require("oauth");
+// const session = require("express-session");
+// // const cookieParser = require("cookie-parser");
+// const bodyParser = require("body-parser");
+// // const util = require("util"); // only using promisfy
+// const { promisfy } = require("util");
+// const request = require("request"); // depreciated
+// const got = require("got");
+// const path = require("path");
+// const socketIo = require("socket.io");
+// const http = require("http");
+// require("dotenv/config");
+
+// const app = express();
+// let port = process.env.PORT || 3000;
+// // const post = util.promisify(request.post);
+// const post = promisify(got.post);
+// const get = promisify(got.get);
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// const server = http.createServer(app);
+// const io = socketIo(server);
+
+// const BEARER_TOKEN = process.env.TWITTER_BEARER_TOKEN;
+// const COOKIE_SECRET = process.env.npm_config_cookie_secret || process.env.COOKIE_SECRET
+// const TWITTER_API_KEY = process.env.npm_config_twitter_api_key || process.env.TWITTER_API_KEY
+// const TWITTER_API_SECRET_KEY = process.env.npm_config_twitter_api_secret_key || process.env.TWITTER_API_SECRET_KEY
+
+// let timeout = 0;
+
+// const oauthConsumer = new oauth.OAuth(
+//     'https://twitter.com/oauth/request_token', 
+//     'https://twitter.com/oauth/access_token',
+//     TWITTER_API_KEY,
+//     TWITTER_API_SECRET_KEY,
+//     '1.0A', 
+//     'http://127.0.0.1:3000/twitter/callback', 
+//     'HMAC-SHA1'
+// );
+
+// const oauthGet = 
+
+// const streamURL = new URL(
+//   "https://api.twitter.com/2/tweets/search/stream?tweet.fields=context_annotations&expansions=author_id"
+// );
+// // console.log('streamURL:', streamURL)
+
+// const rulesURL = new URL(
+//   "https://api.twitter.com/2/tweets/search/stream/rules"
+// );
+// // console.log('rulesURL:', rulesURL)
+
+// const errorMessage = {
+//   title: "Please Wait",
+//   detail: "Waiting for new Tweets to be posted...",
+// };
+
+// const authMessage = {
+//   title: "Could not authenticate",
+//   details: [
+//     `Please make sure your bearer token is correct. 
+//       If using Glitch, remix this app and add it to the .env file`,
+//   ],
+//   type: "https://developer.twitter.com/en/docs/authentication",
+// };
+
+// const sleep = async (delay) => {
+//     // console.log('sleep:', sleep)
+//   return new Promise((resolve) => setTimeout(() => resolve(true), delay));
+// };
+
+// app.get("/api/rules", async (req, res) => {
+//   if (!BEARER_TOKEN) {
+//     res.status(400).send(authMessage);
+//   }
+
+//   const token = BEARER_TOKEN;
+//   const requestConfig = {
+//     url: rulesURL,
+//     auth: {
+//       bearer: token,
+//     },
+//     json: true,
+//   };
+
+//   try {
+//     const response = await get(requestConfig);
+
+//     if (response.statusCode !== 200) {
+//       if (response.statusCode === 403) {
+//         res.status(403).send(response.body);
+//       } else {
+//         throw new Error(response.body.error.message);
+//       }
+//     }
+
+//     res.send(response);
+//   } catch (e) {
+//     res.send(e);
+//   }
+// });
+
+// app.post("/api/rules", async (req, res) => {
+//     console.log(post)
+//   if (!BEARER_TOKEN) {
+//     res.status(400).send(authMessage);
+//   }
+
+//   const token = BEARER_TOKEN;
+//   const requestConfig = {
+//     url: rulesURL,
+//     auth: {
+//       bearer: token,
+//     },
+//     json: req.body,
+//   };
+
+//   try {
+//     const response = await post(requestConfig);
+
+//     if (response.statusCode === 200 || response.statusCode === 201) {
+//       res.send(response);
+//     } else {
+//       throw new Error(response);
+//     }
+//   } catch (e) {
+//     res.send(e);
+//   }
+// });
+
+// const streamTweets = (socket, token) => {
+//   let stream;
+
+//   const config = {
+//     url: streamURL,
+//     auth: {
+//       bearer: token,
+//     },
+//     timeout: 31000,
+//   };
+
+//   try {
+//     const stream = request.get(config);
+
+//     stream
+//       .on("data", (data) => {
+//         try {
+//           const json = JSON.parse(data);
+//           if (json.connection_issue) {
+//             socket.emit("error", json);
+//             reconnect(stream, socket, token);
+//           } else {
+//             if (json.data) {
+//               socket.emit("tweet", json);
+//             } else {
+//               socket.emit("authError", json);
+//             }
+//           }
+//         } catch (e) {
+//           socket.emit("heartbeat");
+//         }
+//       })
+//       .on("error", (error) => {
+//         // Connection timed out
+//         socket.emit("error", errorMessage);
+//         reconnect(stream, socket, token);
+//       });
+//   } catch (e) {
+//     socket.emit("authError", authMessage);
+//   }
+// };
+
+// const reconnect = async (stream, socket, token) => {
+//   timeout++;
+//   stream.abort();
+//   await sleep(2 ** timeout * 1000);
+//   streamTweets(socket, token);
+// };
+
+// io.on("connection", async (socket) => {
+//   try {
+//     const token = BEARER_TOKEN;
+//     io.emit("connect", "Client connected");
+//     const stream = streamTweets(io, token);
+//   } catch (e) {
+//     io.emit("authError", authMessage);
+//   }
+// });
+
+// console.log("NODE_ENV is", process.env.NODE_ENV);
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../build")));
+//   app.get("*", (request, res) => {
+//     res.sendFile(path.join(__dirname, "../build", "index.html"));
+//   });
+// } else {
+//   port = 3001;
+// }
+
+// server.listen(port, () => console.log(`Listening on port ${port}`));
+
+
 const express = require("express");
-const oauth = require("oauth");
-const session = require("express-session");
-// const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-// const util = require("util"); // only using promisfy
-const { promisfy } = require("util");
-const request = require("request"); // depreciated
-const got = require("got");
+const util = require("util");
+const request = require("request");
 const path = require("path");
 const socketIo = require("socket.io");
 const http = require("http");
-require("dotenv/config");
 
 const app = express();
-let port = process.env.PORT || 3000;
-// const post = util.promisify(request.post);
-const post = promisify(got.post);
-const get = promisify(got.get);
+let port = process.env.PORT || 3002;
+const post = util.promisify(request.post);
+const get = util.promisify(request.get);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,21 +224,16 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 const BEARER_TOKEN = process.env.TWITTER_BEARER_TOKEN;
-const COOKIE_SECRET = process.env.npm_config_cookie_secret || process.env.COOKIE_SECRET
-const TWITTER_API_KEY = process.env.npm_config_twitter_api_key || process.env.TWITTER_API_KEY
-const TWITTER_API_SECRET_KEY = process.env.npm_config_twitter_api_secret_key || process.env.TWITTER_API_SECRET_KEY
 
 let timeout = 0;
 
 const streamURL = new URL(
   "https://api.twitter.com/2/tweets/search/stream?tweet.fields=context_annotations&expansions=author_id"
 );
-// console.log('streamURL:', streamURL)
 
 const rulesURL = new URL(
   "https://api.twitter.com/2/tweets/search/stream/rules"
 );
-// console.log('rulesURL:', rulesURL)
 
 const errorMessage = {
   title: "Please Wait",
@@ -56,7 +250,6 @@ const authMessage = {
 };
 
 const sleep = async (delay) => {
-    // console.log('sleep:', sleep)
   return new Promise((resolve) => setTimeout(() => resolve(true), delay));
 };
 
@@ -92,7 +285,6 @@ app.get("/api/rules", async (req, res) => {
 });
 
 app.post("/api/rules", async (req, res) => {
-    console.log(post)
   if (!BEARER_TOKEN) {
     res.status(400).send(authMessage);
   }
@@ -190,3 +382,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
+
+ 
